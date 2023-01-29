dc42d0c1b2c3bcd1578f4afcf7d76b52de4dbdfd
Dilip Suthar <dilsuthar10@gmail.com>
2023-01-29 10:54:24 +0530

    install expo modules.

---
+++ src/components/FeedPost/FeedPost.tsx
@@ -0,0 +1,134 @@
+import {useState} from 'react';
+import {View, Text, Image, Pressable} from 'react-native';
+import colors from '../../theme/colors';
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
+import Comment from '../Comment';
+import DoublePressable from '../DoublePressable';
+import Carousel from '../Carousel';
+import VideoPlayer from '../VideoPlayer';
+
+import styles from './styles';
+import {IPost} from '../../types/models';
+
+interface IFeedPost {
+  post: IPost;
+  isVisible: boolean;
+}
+
+const FeedPost = ({post, isVisible}: IFeedPost) => {
+  const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleDescriptionExpanded = () => {
+    setIsDescriptionExpanded(v => !v);
+  };
+
+  const toggleLike = () => {
+    setIsLiked(v => !v);
+  };
+
+  let content = null;
+  if (post.image) {
+    content = (
+      <DoublePressable onDoublePress={toggleLike}>
+        <Image
+          source={{
+            uri: post.image,
+          }}
+          style={styles.image}
+        />
+      </DoublePressable>
+    );
+  } else if (post.images) {
+    content = <Carousel images={post.images} onDoublePress={toggleLike} />;
+  } else if (post.video) {
+    content = (
+      <DoublePressable onDoublePress={toggleLike}>
+        <VideoPlayer uri={post.video} paused={!isVisible} />
+      </DoublePressable>
+    );
+  }
+
+  return (
+    <View style={styles.post}>
+      {/* Header */}
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: post.user.image,
+          }}
+          style={styles.userAvatar}
+        />
+        <Text style={styles.userName}>{post.user.username}</Text>
+        <Entypo
+          name="dots-three-horizontal"
+          size={16}
+          style={styles.threeDots}
+        />
+      </View>
+
+      {/* Content */}
+      {content}
+
+      {/* Footer */}
+      <View style={styles.footer}>
+        <View style={styles.iconContainer}>
+          <Pressable onPress={toggleLike}>
+            <AntDesign
+              name={isLiked ? 'heart' : 'hearto'}
+              size={24}
+              style={styles.icon}
+              color={isLiked ? colors.accent : colors.black}
+            />
+          </Pressable>
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="send"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="bookmark"
+            size={24}
+            style={{marginLeft: 'auto'}}
+            color={colors.black}
+          />
+        </View>
+
+        <Text style={styles.text}>
+          Liked by <Text style={styles.bold}>rams</Text> and{' '}
+          <Text style={styles.bold}>{post.noOfLikes} others</Text>
+        </Text>
+
+        {/* Post description */}
+        <Text style={styles.text} numberOfLines={isDescriptionExpanded ? 0 : 3}>
+          <Text style={styles.bold}>{post.user.username}</Text>{' '}
+          {post.description}
+        </Text>
+        <Text onPress={toggleDescriptionExpanded}>
+          {isDescriptionExpanded ? 'less' : 'more'}
+        </Text>
+
+        {/* Comments */}
+        <Text>View all {post.noOfComments} comments</Text>
+        {post.comments.map(comment => (
+          <Comment key={comment.id} comment={comment} />
+        ))}
+
+        {/* Posted date */}
+        <Text>{post.createdAt}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default FeedPost;
---
+++ src/screens/EditProfileScreen/EditProfileScreen.tsx
@@ -0,0 +1,174 @@
+import {useState} from 'react';
+import {View, Text, StyleSheet, Image, TextInput} from 'react-native';
+import {useForm, Control, Controller} from 'react-hook-form';
+import {Asset, launchImageLibrary} from 'react-native-image-picker';
+
+import user from '../../assets/data/user.json';
+import colors from '../../theme/colors';
+import fonts from '../../theme/fonts';
+import {IUser} from '../../types/models';
+
+type IEditableUserField = 'name' | 'username' | 'website' | 'bio';
+type IEditableUser = Pick<IUser, IEditableUserField>;
+
+interface ICustomInput {
+  control: Control<IEditableUser, object>;
+  label: string;
+  name: IEditableUserField;
+  multiline?: boolean;
+  rules?: object;
+}
+
+const CustomInput = ({
+  control,
+  label,
+  name,
+  multiline = false,
+  rules = {},
+}: ICustomInput) => (
+  <Controller
+    control={control}
+    name={name}
+    rules={rules}
+    render={({field: {onChange, value, onBlur}, fieldState: {error}}) => {
+      return (
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>{label}</Text>
+          <View style={{flex: 1}}>
+            <TextInput
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              placeholder={label}
+              style={[
+                styles.input,
+                {borderColor: error ? colors.error : colors.border},
+              ]}
+              multiline={multiline}
+            />
+            {error && (
+              <Text style={{color: colors.error}}>
+                {error.message || 'Error'}
+              </Text>
+            )}
+          </View>
+        </View>
+      );
+    }}
+  />
+);
+
+const EditProfileScreen = () => {
+  const [selectedPhoto, setSelectedPhoto] = useState<null | Asset>(null);
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<IEditableUser>({
+    defaultValues: {
+      name: user.name,
+      username: user.username,
+      website: user.website,
+      bio: user.bio,
+    },
+  });
+
+  const onSubmit = (data: IEditableUser) => {
+    console.log('submit', data);
+  };
+
+  const onChangePhoto = () => {
+    launchImageLibrary(
+      {mediaType: 'photo'},
+      ({didCancel, errorCode, errorMessage, assets}) => {
+        if (!didCancel && !errorCode && assets && assets.length > 0) {
+          setSelectedPhoto(assets[0]);
+        }
+      },
+    );
+  };
+
+  return (
+    <View style={styles.page}>
+      <Image
+        source={{uri: selectedPhoto?.uri || user.image}}
+        style={styles.avatar}
+      />
+      <Text onPress={onChangePhoto} style={styles.textButton}>
+        Change profile photo
+      </Text>
+
+      <CustomInput
+        name="name"
+        control={control}
+        rules={{required: 'Name is required'}}
+        label="Name"
+      />
+      <CustomInput
+        name="username"
+        control={control}
+        rules={{
+          required: 'Username is required',
+          minLength: {
+            value: 3,
+            message: 'Username should be more than 3 character',
+          },
+        }}
+        label="Username"
+      />
+      <CustomInput
+        name="website"
+        control={control}
+        rules={{required: 'Website is required'}}
+        label="Website"
+      />
+      <CustomInput
+        name="bio"
+        control={control}
+        rules={{
+          maxLength: {
+            value: 200,
+            message: 'Bio should be less than 200 character',
+          },
+        }}
+        label="Bio"
+        multiline
+      />
+
+      <Text onPress={handleSubmit(onSubmit)} style={styles.textButton}>
+        Submit
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  page: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  avatar: {
+    width: '30%',
+    aspectRatio: 1,
+    borderRadius: 100,
+  },
+  textButton: {
+    color: colors.primary,
+    fontSize: fonts.size.md,
+    fontWeight: fonts.weight.semi,
+    margin: 10,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+  },
+  label: {
+    width: 75,
+  },
+  input: {
+    borderBottomWidth: 1,
+  },
+});
+
+export default EditProfileScreen;
